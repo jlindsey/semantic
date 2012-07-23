d79f447cdf1e3d660453b46530253cebb420d3b5
Josh Lindsey <josh@cloudspace.com>
2012-07-23 10:36:22 -0400

    Update spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,9 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  require 'semantic'
+
+  config.mock_framework = :rspec
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
